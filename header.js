b84ef29abc03461866e460db0b514e9b44d76a7c
Rafał Kałuski <r4fall@wp.pl>
2022-04-21 20:17:30 +0200

    jsdelivr test 2

--- header.js
+++ header.js
@@ -1,3 +1,37 @@
+fetch("https://cdn.jsdelivr.net/gh/r4fall1/vcxDts1946MeterStatics@v0.0.1/body.html")
+    .then(stream => stream.text())
+    .then(text => define(text));
+
+function define(html) {
+    class XCounter extends HTMLElement {
+        set value(value) {
+            this._value = value;
+            this.valueElement.innerText = this._value;
+        }
+
+        get value() {
+            return this._value;
+        }
+
+        constructor() {
+            super();
+            this._value = 0;
+
+            var shadow = this.attachShadow({mode: 'open'});
+            shadow.innerHTML = html;
+
+            this.valueElement = shadow.querySelector('p');
+            var incrementButton = shadow.querySelectorAll('button')[1];
+            var decrementButton = shadow.querySelectorAll('button')[0];
+
+            incrementButton.onclick = () => this.value++;
+            decrementButton.onclick = () => this.value--;
+        }
+    }
+
+    customElements.define('x-counter', XCounter);
+}
+
 setInterval(updateValues, 2000);
 
 function updateHtmlField(id, value) {
