cc5951ca9149a9612d50abe4c1deb822f713b8be
Rafał Kałuski <r4fall@wp.pl>
2022-04-26 00:58:40 +0200

    Dummy html import

--- component/energyMeter/energy-meter.js
+++ component/energyMeter/energy-meter.js
@@ -1,77 +1,40 @@
-fetch("https://cdn.jsdelivr.net/gh/r4fall1/vcxDts1946MeterWeb@v0/component/energyMeter/template.html")
+fetch("https://cdn.jsdelivr.net/gh/r4fall1/vcxDts1946MeterWeb@v0.0.8/component/energyMeter/template.html")
     .then(stream => stream.text())
-    .then(text => define(text));
+    .then(html => define(html));
 
 function define(html) {
-    class XBody extends HTMLElement {
-        constructor() {
-            super();
+    $("#energy-meter").html(html)
+}
 
-            let shadow = this.attachShadow({mode: 'open'});
-            shadow.innerHTML = html;
-        }
-
-        updateHtmlField(id, value) {
-            if (value !== 'nan') {
-                this.shadowRoot.getElementById(id).innerHTML = value
-            }
-        }
-
-        msToTime(s) {
-            function pad(n, z) {
-                z = z || 2;
-                return ('00' + n).slice(-z);
-            }
-
-            let ms = s % 1000;
-            s = (s - ms) / 1000;
-            let secs = s % 60;
-            s = (s - secs) / 60;
-            let mins = s % 60;
-            let hrs = (s - mins) / 60;
-
-            return pad(hrs) + ':' + pad(mins) + ':' + pad(secs);
-        }
-
-        updateValues() {
-            let app = this;
-            let xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                    console.log("params = " + this.responseText);
-                    const vals = this.responseText.split(';');
-                    app.updateHtmlField("phase1Voltage", vals[0]);
-                    app.updateHtmlField("phase2Voltage", vals[1]);
-                    app.updateHtmlField("phase3Voltage", vals[2]);
-                    app.updateHtmlField("phase1ToPhase2Voltage", vals[3]);
-                    app.updateHtmlField("phase2ToPhase3Voltage", vals[4]);
-                    app.updateHtmlField("phase3ToPhase1Voltage", vals[5]);
-                    app.updateHtmlField("waterTemperature", vals[6]);
-                    app.updateHtmlField("heaterOperatingTime", app.msToTime(vals[7]));
-                    app.updateHtmlField("heaterStatus", vals[8] > 0 ? "ON" : "OFF");
-                }
-            }
-            xhttp.open("GET", "/params", false);
-            xhttp.send();
-        }
-
-        heaterTest() {
-            let app = this;
-            let xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                    console.log("status = " + this.responseText);
-                    app.updateValues();
-                }
-            }
-            xhttp.open("GET", "/test", false);
-            xhttp.send();
-        }
+function msToTime(s) {
+    function pad(n, z) {
+        z = z || 2;
+        return ('00' + n).slice(-z);
     }
 
-    customElements.define('x-body', XBody);
+    let ms = s % 1000;
+    s = (s - ms) / 1000;
+    let secs = s % 60;
+    s = (s - secs) / 60;
+    let mins = s % 60;
+    let hrs = (s - mins) / 60;
 
-    let meter = document.createElement('x-body');
-    setInterval(meter.updateValues, 5000)
-    meter.updateValues()
+    return pad(hrs) + ':' + pad(mins) + ':' + pad(secs);
 }
+
+function updateValues() {
+    $.get('/energy-meter/get', function(data) {
+        const vals = data.split(';')
+        $('#phase1Voltage').html(vals[0])
+        $('#phase2Voltage').html(vals[1])
+        $('#phase3Voltage').html(vals[2])
+        $('#phase1ToPhase2Voltage').html(vals[3])
+        $('#phase2ToPhase3Voltage').html(vals[4])
+        $('#phase3ToPhase1Voltage').html(vals[5])
+        $('#waterTemperature').html(vals[6])
+        $('#heaterOperatingTime').html(msToTime(vals[7]))
+        $('#heaterStatus').html(vals[8] > 0 ? "ON" : "OFF")
+    });
+}
+
+setInterval(updateValues, 2000)
