ead4d34986dd29ffc53778cbd10c649c00c8ebbb
Rafał Kałuski <rafal.kaluski@asseco.pl>
2022-11-18 23:47:13 +0100

    v1.1.8

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const packageJson = fs.readFileSync('./package.json')
 const version = JSON.parse(packageJson).version || 0
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.jsdelivr.net/gh/r4fall1/vcxDts1946MeterWeb@v' + version + '/prd/' : '',
+    publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.jsdelivr.net/gh/r4fall1/vcxDts1946MeterWeb@v' + version + '/prd' : '',
     configureWebpack: (config) => {
         config.output.filename = '[name].js';
         config.output.chunkFilename = '[name].js';
